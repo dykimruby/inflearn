c134c235113476d466a62201cca23686c4c3245d
dykimruby <130230167+dykimruby@users.noreply.github.com>
2024-06-04 14:53:59 +0900

    9.1 finish

---
+++ section07/src/components/Even.jsx
@@ -0,0 +1,14 @@
+import { userEffect } from "react";
+
+const Even = () => {
+  userEffect(() =>{
+    //클린업, 정리함수
+    return () => {
+      console.log("unmount");
+    };
+  }, []);
+
+  return <div>짝수입니다</div>;
+};
+
+export default Even;
